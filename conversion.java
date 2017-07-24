7a35f5cc764dd2d60d9e44babdbf86cb71fbef83
Sownthiryamuthusamy96 <sownthiryamuthusamy@gmail.com>
2017-07-24 22:58:09 +0530

    Create conversion.java

---
+++ conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+public static void main(String[] args)
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String str=br.readLine();
+int n=Integer.parseInt(str);
+System.out.println(n);
+}
+}
